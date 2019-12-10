a6198b9440dce949f8dc49867ee3dbcc11d93382
Michael Lee <michl@amazon.com>
2019-12-10 14:07:42 -0800

    Apply formatting
    * Discussed published style guides with other X-Ray devs and selected Google's
      Java Style
    * Adding missing javadocs to comply with said style
    * All other changes are whitespace / ordering only

--- sdk/src/main/java/com/amazonaws/xray/opentelemetry/tracing/RecorderBackedTracer.java
+++ sdk/src/main/java/com/amazonaws/xray/opentelemetry/tracing/RecorderBackedTracer.java
@@ -13,79 +13,80 @@ import io.opentelemetry.trace.Tracer;
 
 /**
  * A tracer backed by an AWS X-Ray recorder.
+ *
  * @see io.opentelemetry.trace.Tracer
  */
 public class RecorderBackedTracer implements Tracer {
 
-    private static final HttpTraceContext HTTP_TRACE_CONTEXT = new HttpTraceContext();
-    private AWSXRayRecorder recorder;
-    private Span currentSpan = null;
-    private Entity currentEntity = null;
+  private static final HttpTraceContext HTTP_TRACE_CONTEXT = new HttpTraceContext();
+  private AWSXRayRecorder recorder;
+  private Span currentSpan = null;
+  private Entity currentEntity = null;
 
-    /**
-     * Create a tracer using a given recorder.
-     * @param recorder the recorder
-     * @return a tracer
-     */
-    public static RecorderBackedTracer fromRecorder(AWSXRayRecorder recorder) {
-        return new RecorderBackedTracer(recorder);
+  private RecorderBackedTracer(AWSXRayRecorder recorder) {
+    this.recorder = recorder;
+  }
+
+  /**
+   * Create a tracer using a given recorder.
+   *
+   * @param recorder the recorder
+   * @return a tracer
+   */
+  public static RecorderBackedTracer fromRecorder(AWSXRayRecorder recorder) {
+    return new RecorderBackedTracer(recorder);
+  }
+
+  /**
+   * {@inheritDoc}
+   * This implementation will automatically update the active span based on changes to the X-Ray
+   * recorder but otherwise conforms to OpenTelemetry semantics when creating spans.
+   */
+  @Override
+  public Span getCurrentSpan() {
+    if (currentSpan == null) {
+      return DefaultSpan.getInvalid();
+    } else {
+      //Reflect the recorder's current entity changing in response to X-Ray calls
+      Entity recorderCurrentEntity = recorder.getTraceEntity();
+      if (recorderCurrentEntity != null && !recorderCurrentEntity.equals(currentEntity)) {
+        currentEntity = recorderCurrentEntity;
+        currentSpan = EntitySpan.fromEntity(currentEntity);
+      }
+    }
+    return currentSpan;
+  }
+
+  @Override
+  public Scope withSpan(final Span span) {
+    if (span instanceof EntitySpan) {
+      final EntitySpan adapter = (EntitySpan) span;
+      currentSpan = span;
+      recorder.setTraceEntity(adapter.getXrayEntity());
     }
 
-    private RecorderBackedTracer(AWSXRayRecorder recorder) {
-        this.recorder = recorder;
-    }
+    //Just a rough implementation. There's some talk of separating scope management from
+    //existing gRPC influenced deps. Need to better integrate this and X-Ray Recorder's
+    //scope managerment.
+    return new Scope() {
+      @Override
+      public void close() {
+      }
+    };
+  }
 
-    @Override
-    /**
-     * {@inheritDoc}
-     *
-     * This implementation will automatically update the active span based on changes to the X-Ray recorder but
-     * otherwise conforms to OpenTelemetry semantics when creating spans.
-     */
-    public Span getCurrentSpan() {
-        if(currentSpan == null) {
-            return DefaultSpan.getInvalid();
-        } else {
-            //Reflect the recorder's current entity changing in response to X-Ray calls
-            Entity recorderCurrentEntity = recorder.getTraceEntity();
-            if (recorderCurrentEntity != null && !recorderCurrentEntity.equals(currentEntity)) {
-                currentEntity = recorderCurrentEntity;
-                currentSpan = EntitySpan.fromEntity(currentEntity);
-            }
-        }
-        return currentSpan;
-    }
+  @Override
+  public Span.Builder spanBuilder(final String name) {
+    return EntitySpanBuilder.create(name, recorder, getCurrentSpan());
+  }
 
-    @Override
-    public Scope withSpan(final Span span) {
-        if(span instanceof EntitySpan) {
-            final EntitySpan adapter = (EntitySpan) span;
-            currentSpan = span;
-            recorder.setTraceEntity(adapter.getXrayEntity());
-        }
+  @Override
+  public BinaryFormat<SpanContext> getBinaryFormat() {
+    return null;
+  }
 
-        //Just a rough implementation. There's some talk of separating scope management from
-        //existing gRPC influenced deps. Need to better integrate this and X-Ray Recorder's
-        //scope managerment.
-        return new Scope() {
-            @Override
-            public void close() {
-            }
-        };
-    }
-
-    @Override
-    public Span.Builder spanBuilder(final String name) {
-        return EntitySpanBuilder.create(name, recorder, getCurrentSpan());
-    }
-
-    @Override
-    public BinaryFormat<SpanContext> getBinaryFormat() {
-        return null;
-    }
-
-    @Override
-    public HttpTextFormat<SpanContext> getHttpTextFormat() {
-        return HTTP_TRACE_CONTEXT;
-    }
+  @Override
+  public HttpTextFormat<SpanContext> getHttpTextFormat() {
+    return HTTP_TRACE_CONTEXT;
+  }
 }
